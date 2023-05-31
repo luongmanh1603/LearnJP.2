4ba74d3df2787d1dd03fd8e9d76e3922c5455555
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-05-31 12:43:31 +0700

    for

---
+++ src/caulenhdkhien/VD_For.java
@@ -0,0 +1,13 @@
+package caulenhdkhien;
+
+public class VD_For {
+    public static void main(String[] args) {
+        // in ra bang cuu chuong
+        for (int i =2; i<=9;i++){
+            for (int j = 1;j<=10;j++){
+                System.out.println(i+"x"+j+"="+(i*j));
+            }
+            System.out.println("----");
+
+        }    }
+}
