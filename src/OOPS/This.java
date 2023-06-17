8dc3d255e1e0d5370a2f641b995445f631e97cdf
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-06-17 23:34:20 +0700

    this

---
+++ src/OOPS/This.java
@@ -0,0 +1,31 @@
+package OOPS;
+
+public class This {
+    // tham chieu toi bien insstance cua lop hien tai, neu trung ten voi constructor
+    // goi constructor cua lop hien tai
+    int id;
+    String name;
+    String city;
+
+    public This(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    This(int id, String name, String city) {
+        this(id,name);
+        this.city = city;
+    }
+    void display(){
+        System.out.println(id+" "+name +" "+ city);
+
+
+    }
+
+    public static void main(String[] args) {
+        This t = new This(2,"dai");
+        This o = new This(222,"manh","hanoi");
+        o.display();
+        t.display();
+    }
+}
