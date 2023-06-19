cc50d06fc39e0da27607aae9e898f3d9fb04cc28
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-06-19 23:40:13 +0700

    final

---
+++ src/OOPS/Final.java
@@ -0,0 +1,19 @@
+package OOPS;
+
+public class Final {
+    // duoc sd de han che nguoi dung
+    // bien final khong the thay doi
+    // khong the ghi de phthuc final,nhung co the thua ke
+    // khong the thua ke lop final
+    // b
+
+    final int a = 100;
+     void in(){
+        int a = 10;
+    }
+
+    public static void main(String[] args) {
+        Final f = new Final();
+        f.in();
+    }
+}
