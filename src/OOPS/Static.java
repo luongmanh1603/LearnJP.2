a100d34c1f1880fddbd17b85526a00de50a69860
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-06-21 21:50:59 +0700

    static

--- src/OOPS/Static.java
+++ src/OOPS/Static.java
@@ -1,10 +1,17 @@
 package OOPS;
 
 public class Static {
+   static int a = 0;
     int id;
     String school;
     static int room = 401;
 
+    static void change(){
+        //pth static co the truy cap va thay doi bien static
+        // thuoc lop chu khong thuoc instance of lop
+        // khong can tao obj
+        room = 601;
+    }
     public Static(int id, String school) {
         this.id = id;
         this.school = school;
@@ -17,10 +24,12 @@ public class Static {
     }
 
     public static void main(String[] args) {
+        Static.change();
         Static p = new Static(1,"FPT");
         p.display();
         Static q = new Static(2,"HANU");
         q.display();
+        System.out.println(a);
 
     }
 }
